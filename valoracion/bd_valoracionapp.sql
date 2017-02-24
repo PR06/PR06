8a33e4b8d038d4f4a851d043e10e45d7ef32ea59
MiquelGomez <100005548.joan23@fje.edu>
2017-02-24 20:02:54 +0100

    profesor_proyecto +sql
    he añadido una tabla intermedia entre profesorproyecto y profesor

--- valoracion/bd_valoracionapp.sql
+++ valoracion/bd_valoracionapp.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 23, 2017 at 05:20 PM
+-- Generation Time: Feb 24, 2017 at 08:01 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 7.0.6
 
@@ -76,6 +76,18 @@ CREATE TABLE `tbl_preguntapublico` (
   `prpu_pregunta` varchar(200) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `tbl_preguntapublico`
+--
+
+INSERT INTO `tbl_preguntapublico` (`prpu_id`, `prpu_tipo`, `prpu_pregunta`) VALUES
+(1, 'oral', '1. T''ha quedat una idea clara de la part que ha exposat.'),
+(2, 'oral', '2. Com valores la seva expressió oral.'),
+(3, 'oral', '3. Creus que la presentació està ben estructurada..'),
+(4, 'contenido', '4. Com vlores la qualitat del power-point, flash, etc que s''ha fet servir a l''exposició'),
+(5, 'contenido', '5. T''ha quedat clar el contigut del projecte.'),
+(6, 'contenido', '6. Com valores la qualitat del projecte exposat.');
+
 -- --------------------------------------------------------
 
 --
@@ -87,6 +99,21 @@ CREATE TABLE `tbl_preguntatribunal` (
   `prtri_tipo` enum('oral','contenido') NOT NULL,
   `prtri_pregunta` varchar(300) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `tbl_preguntatribunal`
+--
+
+INSERT INTO `tbl_preguntatribunal` (`prtri_id`, `prtri_tipo`, `prtri_pregunta`) VALUES
+(1, 'oral', '1. Estructura de la presentació. (presentació personal, introducció, presentació del projecte, conclusions, preguntes, comiat adequat)'),
+(2, 'oral', '2. Temps utilitzat. (duració total, duració de cada part de l''exposició)'),
+(3, 'oral', '3. Expressió oral. (to de veu, claredar oral, ritme, vocabulari  adequat ...)'),
+(4, 'oral', '4. Presencia adequada. (presencia física, moviment corporal, etc ...)'),
+(5, 'contenido', '5. Material utilizat en l''exposició oral. (PowerPoint, fotocopies, vídeo, etc ...)'),
+(6, 'contenido', '6. Desenvolupament i procès del projecte.'),
+(7, 'contenido', '7. Conclusions ben raonades.'),
+(8, 'contenido', '8. Respostes a les preguntes del tribunal.'),
+(9, 'contenido', '9. Qualitat del producte obtingut segons l''exposició.');
 
 -- --------------------------------------------------------
 
@@ -106,7 +133,8 @@ CREATE TABLE `tbl_profesor` (
 --
 
 INSERT INTO `tbl_profesor` (`profesor_id`, `profesor_nombre`, `profesor_apellido`, `profesor_correo`) VALUES
-(1, 'David', 'Marin', 'david.marin@fje.edu');
+(1, 'David', 'Marin', 'david.marin@fje.edu'),
+(2, 'Sergio', 'Jimenez', 'sjimenes@gmail.com');
 
 -- --------------------------------------------------------
 
@@ -142,7 +170,6 @@ INSERT INTO `tbl_proyecto` (`proyecto_id`, `proyecto_nombre`, `proyecto_imagen`,
 
 CREATE TABLE `tbl_proyectoprofesor` (
   `pp_id` int(11) NOT NULL,
-  `pp_profesorid` int(11) NOT NULL,
   `pp_proyectoid` int(11) NOT NULL,
   `pp_usuario` varchar(15) NOT NULL,
   `pp_password` varchar(15) NOT NULL
@@ -152,8 +179,9 @@ CREATE TABLE `tbl_proyectoprofesor` (
 -- Dumping data for table `tbl_proyectoprofesor`
 --
 
-INSERT INTO `tbl_proyectoprofesor` (`pp_id`, `pp_profesorid`, `pp_proyectoid`, `pp_usuario`, `pp_password`) VALUES
-(1, 1, 2, 'profesor', 'profe');
+INSERT INTO `tbl_proyectoprofesor` (`pp_id`, `pp_proyectoid`, `pp_usuario`, `pp_password`) VALUES
+(1, 2, 'profesor', 'profe'),
+(2, 1, 'prueba', 'prueba');
 
 -- --------------------------------------------------------
 
@@ -184,6 +212,27 @@ CREATE TABLE `tbl_publiconota` (
   `pn_alumnoid` int(11) NOT NULL,
   `pn_nota` decimal(4,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_tribunal`
+--
+
+CREATE TABLE `tbl_tribunal` (
+  `tribunal_id` int(11) NOT NULL,
+  `tribunal_profesorid` int(11) NOT NULL,
+  `tribunal_ppid` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Dumping data for table `tbl_tribunal`
+--
+
+INSERT INTO `tbl_tribunal` (`tribunal_id`, `tribunal_profesorid`, `tribunal_ppid`) VALUES
+(1, 1, 1),
+(2, 2, 1),
+(3, 1, 2);
 
 -- --------------------------------------------------------
 
@@ -252,6 +301,12 @@ ALTER TABLE `tbl_publico`
   ADD PRIMARY KEY (`publico_id`);
 
 --
+-- Indexes for table `tbl_tribunal`
+--
+ALTER TABLE `tbl_tribunal`
+  ADD PRIMARY KEY (`tribunal_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -266,15 +321,20 @@ ALTER TABLE `tbl_admin`
 ALTER TABLE `tbl_alumno`
   MODIFY `alumno_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
+-- AUTO_INCREMENT for table `tbl_preguntapublico`
+--
+ALTER TABLE `tbl_preguntapublico`
+  MODIFY `prpu_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+--
 -- AUTO_INCREMENT for table `tbl_preguntatribunal`
 --
 ALTER TABLE `tbl_preguntatribunal`
-  MODIFY `prtri_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `prtri_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
 --
 -- AUTO_INCREMENT for table `tbl_profesor`
 --
 ALTER TABLE `tbl_profesor`
-  MODIFY `profesor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `profesor_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `tbl_proyecto`
 --
@@ -284,12 +344,17 @@ ALTER TABLE `tbl_proyecto`
 -- AUTO_INCREMENT for table `tbl_proyectoprofesor`
 --
 ALTER TABLE `tbl_proyectoprofesor`
-  MODIFY `pp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `pp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `tbl_publico`
 --
 ALTER TABLE `tbl_publico`
   MODIFY `publico_id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `tbl_tribunal`
+--
+ALTER TABLE `tbl_tribunal`
+  MODIFY `tribunal_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- valoracion/php/profesor_proyecto.php
+++ valoracion/php/profesor_proyecto.php
@@ -4,17 +4,71 @@
 
 	include 'conexion.php';
 
-	$sql_proyectos = "SELECT * FROM tbl_proyectos";
+	$sql_proyectos = "SELECT * FROM tbl_proyecto LEFT JOIN tbl_profesor ON tbl_profesor.profesor_id = tbl_proyecto.proyecto_tutor ORDER BY proyecto_fecha DESC";
+	$proyectos = mysqli_query($conexion, $sql_proyectos);
 
 ?>
 <form>
+	Ordenar proyecte per:
 	<select name="filtro">
-		<option>Fecha1</option>
+		<option>Data</option>
+		<option>A-Z</option>
 	</select>
-	<button></button>
 </form>
 <?php 
 
+	if (mysqli_num_rows($proyectos)>0){
+		while ($proyecto = mysqli_fetch_array($proyectos)) {
+			$sql_alumnos = "SELECT * FROM tbl_proyecto INNER JOIN tbl_alumno ON tbl_alumno.alumno_proyectoid = tbl_proyecto.proyecto_id WHERE alumno_proyectoid = '$proyecto[proyecto_id]'";
+			$alumnos = mysqli_query($conexion, $sql_alumnos);
+
+			$sql_profesores = "SELECT * FROM tbl_proyecto INNER JOIN tbl_proyectoprofesor ON tbl_proyectoprofesor.pp_proyectoid = tbl_proyecto.proyecto_id INNER JOIN tbl_tribunal ON tbl_tribunal.tribunal_ppid = tbl_proyectoprofesor.pp_id INNER JOIN tbl_profesor ON tbl_profesor.profesor_id = tbl_tribunal.tribunal_profesorid WHERE pp_proyectoid = '$proyecto[proyecto_id]'";
+			$profesores = mysqli_query($conexion, $sql_profesores);
+
+			echo "<div class='profesor_proyecto'>";
+				echo "<div class='proyecto_nombre'>";
+					echo "<h3><b>Projecte:</b> $proyecto[proyecto_nombre]</h3>";
+				echo "</div>";
+				echo "<div class='project'>";
+					echo "<div class='profesor_imagen'>";
+						echo "<img src='$proyecto[proyecto_imagen]'/>";
+					echo "</div>";
+					echo "<div class='proyecto_info'>";
+						echo "<b>Membres:</b> ";
+						while ($alumno = mysqli_fetch_array($alumnos)) {
+							echo "$alumno[alumno_nombre] $alumno[alumno_apellido], ";
+						}
+							echo "<br/>";
+						echo "<b>Tutor:</b> $proyecto[profesor_nombre] $proyecto[profesor_apellido]";
+							echo "<br/>";
+						echo "<b>Tribunal:</b> ";
+						while ($profesor = mysqli_fetch_array($profesores)) {
+							echo "$profesor[profesor_nombre] $profesor[profesor_apellido], ";
+							$pp_id = $profesor['pp_id'];
+						}
+							echo "<br/>";
+						echo "<b>Dia i hora:</b> $proyecto[proyecto_fecha]";
+
+						echo "<div class='valorar'>";
+							if ($pp_id == $id){
+								echo "<a style='color:green;' href='#'>Valorar</a>";
+							} else {
+								echo "<a style='color:red' href='#'>Valorar</a>";
+							}
+							echo "</div>";
+
+					echo "</div>";
+
+				echo "</div>";
+
+			echo "</div>";
+
+		}
+
+
+	} else {
+		echo "No hi ha cap projecte";
+	}
 
 	
 		
